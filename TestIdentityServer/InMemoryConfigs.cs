567f79f8f256c6a72935732695722cb2f7bfe40f
Mohammed <tuniis@live.com>
2019-11-19 22:35:17 -0600

    Initial version

---
+++ TestIdentityServer/InMemoryConfigs.cs
@@ -0,0 +1,144 @@
+﻿namespace TestIdentityServer
+{
+    using System.Collections.Generic;
+    using System.Security.Claims;
+
+    using IdentityServer4;
+    using IdentityServer4.Models;
+    using IdentityServer4.Test;
+
+    /// <summary>
+    /// The in memory configurations.
+    /// </summary>
+    public class InMemoryConfigs
+    {
+        /// <summary>
+        /// The identity resources.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new IdentityResource[]
+                       {
+                           new IdentityResources.OpenId(), new IdentityResources.Profile(),
+                           new IdentityResources.Phone(), new IdentityResources.Email(), new IdentityResources.Address()
+                       };
+        }
+
+        /// <summary>
+        /// The API resources.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+                       {
+                           new ApiResource("TestApp", "My Test Application")
+                               {
+                                   UserClaims = new[]
+                                                    {
+                                                        ClaimTypes.Email, ClaimTypes.HomePhone, ClaimTypes.MobilePhone,
+                                                        ClaimTypes.OtherPhone, ClaimTypes.Role, "Profile", "CanCreateProfile",
+                                                        "CanViewProfile"
+                                                    }
+                               }
+                       };
+        }
+
+        /// <summary>
+        /// The clients.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public static IEnumerable<Client> Clients()
+        {
+            return new[]
+                       {
+                           new Client
+                               {
+                                   ClientId = "TestApp",
+                                   ClientSecrets = new[] { new Secret("secret".Sha256()) },
+                                   AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                                   AllowedScopes = new[]
+                                                       {
+                                                           IdentityServerConstants.StandardScopes.OpenId,
+                                                           IdentityServerConstants.StandardScopes.Profile,
+                                                           IdentityServerConstants.StandardScopes.Email,
+                                                           IdentityServerConstants.StandardScopes.Phone,
+                                                           IdentityServerConstants.StandardScopes.Email,
+                                                           IdentityServerConstants.StandardScopes.Address,
+                                                       },
+                                   AllowOfflineAccess = true,
+                                   AllowAccessTokensViaBrowser = true,
+                                   //RedirectUris = { "" },
+                                   //PostLogoutRedirectUris = { "" }                    
+                               },
+                           new Client
+                               {
+                                   ClientId = "TestApp_implicit",
+                                   ClientSecrets = new[] { new Secret("secret".Sha256()) },
+                                   AllowedGrantTypes = GrantTypes.Implicit,
+                                   AllowedScopes = new[]
+                                                       {
+                                                           IdentityServerConstants.StandardScopes.OpenId,
+                                                           IdentityServerConstants.StandardScopes.Profile,
+                                                           IdentityServerConstants.StandardScopes.Email,
+                                                           IdentityServerConstants.StandardScopes.Phone,
+                                                           IdentityServerConstants.StandardScopes.Email,
+                                                           IdentityServerConstants.StandardScopes.Address
+                                                       },
+                                   AllowAccessTokensViaBrowser = true,
+                                   RedirectUris = new[] { "http://localhost:8406/signin-oidc" },
+                                   PostLogoutRedirectUris = new[] { "http://localhost:8406/signout-callback-oidc" }
+                               }
+                       };
+        }
+
+        /// <summary>
+        /// The users.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable{T}"/>.
+        /// </returns>
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+                       {
+                           new TestUser
+                               {
+                                   SubjectId = "1",
+                                   Username = "testadminuser",
+                                   Password = "password",
+                                   IsActive = true,
+                                   Claims = new List<Claim>()
+                                                {
+                                                    new Claim(ClaimTypes.Role, "Admin"),
+                                                    new Claim(ClaimTypes.MobilePhone, "214-000-0000"),
+                                                    new Claim(ClaimTypes.Email, "test.admin@test.com"),
+                                                    new Claim("Profile", "CanCreateProfile"),
+                                                    new Claim("Profile", "CanViewProfile")
+                                                }
+                               },
+                           new TestUser
+                               {
+                                   SubjectId = "2",
+                                   Username = "testuser",
+                                   Password = "password",
+                                   IsActive = true,
+                                   Claims = new List<Claim>()
+                                                {
+                                                    new Claim(ClaimTypes.Role, "NonAdmin"),
+                                                    new Claim(ClaimTypes.MobilePhone, "469-000-0000"),
+                                                    new Claim(ClaimTypes.Email, "test.user@test.com"),
+                                                    new Claim("Profile", "CanViewProfile")
+                                                }
+                               },
+                       };
+        }
+    }
+}
---
+++ TestIdentityServer/Startup.cs
@@ -0,0 +1,84 @@
+﻿namespace TestIdentityServer
+{
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// The startup.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">
+        /// The configuration.
+        /// </param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// The configure services.
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">
+        /// The services.
+        /// </param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // .AddSigningCredential(new X509Certificate2(@".\certificates\test.pfx", "password"))
+            services.AddLogging(
+                    loggingBuilder =>
+                        {
+                            loggingBuilder.AddConsole();
+                            loggingBuilder.AddDebug();
+                        }).AddIdentityServer().AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(InMemoryConfigs.IdentityResources())
+                .AddInMemoryApiResources(InMemoryConfigs.ApiResources()).AddInMemoryClients(InMemoryConfigs.Clients())
+                .AddTestUsers(InMemoryConfigs.Users().ToList());
+        }
+
+        /// <summary>
+        /// The configure. This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">
+        /// The app.
+        /// </param>
+        /// <param name="env">
+        /// The env.
+        /// </param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Allows IdentityServer to start intercepting routes and handle requests.
+            app.UseIdentityServer();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}

5dfa9e05988726a85ddf607f7bf9dcf050024ea5
LiteObject <Lite.Object@gmail.com>
2023-01-05 16:53:05 -0600

    fixed runtime issues

--- Demo.IdentityServer/Program.cs
+++ Demo.IdentityServer/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
 
 namespace Demo.IdentityServer
 {
@@ -7,13 +9,38 @@ namespace Demo.IdentityServer
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            return WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            // .AddSigningCredential(new X509Certificate2(@".\certificates\test.pfx", "password"))
+
+            _ = builder.Services
+                .AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryIdentityResources(InMemoryConfigs.IdentityResources())
+                .AddInMemoryApiResources(InMemoryConfigs.ApiResources())
+                .AddInMemoryClients(InMemoryConfigs.Clients())
+                .AddInMemoryApiScopes(InMemoryConfigs.ApiScopes)
+                .AddTestUsers(InMemoryConfigs.Users().ToList());
+
+
+            WebApplication app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                _ = app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                _ = app.UseHsts();
+            }
+
+            // Allows IdentityServer to start intercepting routes and handle requests.
+            _ = app.UseIdentityServer();
+
+            _ = app.UseHttpsRedirection();
+
+            app.Run();
         }
     }
 }

f4beaf5cbda48921f7153e2c4fc7430e8c70846f
LiteObject <Lite.Object@gmail.com>
2023-01-06 15:29:15 -0600

    some code cleanup

--- Demo.Api.One/Controllers/WeatherForecastController.cs
+++ Demo.Api.One/Controllers/WeatherForecastController.cs
@@ -18,15 +18,10 @@ namespace Demo.Api.One.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
-        private static readonly string[] scopeRequiredByApi = new string[] { "demoapi.one.read" };
+        // private static readonly string[] scopeRequiredByApi = new string[] { "demoapi.one.read" };
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
